0a86017f447a9c48f159f90190d642f96159090d
BbillahrariBBr <mb.billah8@gmail.com>
2023-02-23 14:43:20 +0600

    2.1 done

---
+++ Module-2/2.1/think-in-a-redux-way-1.2/src/redux/counter/action.js
@@ -0,0 +1,17 @@
+import { INCREMENT } from "./actionType";
+import { DECREMENT } from "./actionType";
+export const increment = (value) => {
+
+    return {
+        type: INCREMENT,
+        payload: value,
+    };
+}
+
+export const decrement = (value) => {
+    return {
+        type: DECREMENT,
+        payload: value,
+
+    };
+}
