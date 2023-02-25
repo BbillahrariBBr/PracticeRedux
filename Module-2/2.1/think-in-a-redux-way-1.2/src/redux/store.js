9ae3ed2564c07d67d19349c439654b458064ea86
BbillahrariBBr <mb.billah8@gmail.com>
2023-02-25 16:34:55 +0600

    2.9 done middleware

--- Module-2/2.1/think-in-a-redux-way-1.2/src/redux/store.js
+++ Module-2/2.1/think-in-a-redux-way-1.2/src/redux/store.js
@@ -1,7 +1,23 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+// create our first middle ware
+const myLogger = (store) => (next) => (action) => {
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+    const upcomingState = [action].reduce(rootReducer, store.getState());
+    const upcomingStateNext = [action].reduce(rootReducer, upcomingState);
+
+    console.log(`Upcoming State: ${JSON.stringify(upcomingState)}`);
+    console.log(`Upcoming State Next: ${JSON.stringify(upcomingStateNext)}`);
+
+    // pass action
+    return next(action);
+
+}
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 // const dstore = createStore(dynamicCounterReducer);
 
 export default store;
