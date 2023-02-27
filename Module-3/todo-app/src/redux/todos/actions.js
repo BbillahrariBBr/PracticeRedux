3964fbc4f9f335f9bd3ab23b257ed1d33e919241
BbillahrariBBr <mb.billah8@gmail.com>
2023-02-27 12:12:40 +0600

    3.2 action creators done

---
+++ Module-3/todo-app/src/redux/todos/actions.js
@@ -0,0 +1,57 @@
+import {
+    ADDED,
+    TOGGLED,
+    COLORSELECTED,
+    DELETED,
+    ALLCOMPLETED,
+    CLEARCOMPLETED
+}
+    from "./actionTypes";
+
+// actions creators
+export const added = (todoText) => {
+    return {
+        type: ADDED,
+        payload: todoText,
+    }
+}
+
+export const toggled = (todoId) => {
+    return {
+        type: TOGGLED,
+        payload: todoId,
+    };
+}
+
+export const colorSelected = (todoId, color) => {
+    return {
+        type: COLORSELECTED,
+        payload: {
+            // color: color,
+            // todoId: todoId,
+            // name and property all same so we can as below
+            todoId,
+            color,
+
+        }
+    };
+}
+
+export const deleted = (todoId) => {
+    return {
+        type: DELETED,
+        payload: todoId,
+    };
+}
+
+export const allCompleted = () => {
+    return {
+        type: ALLCOMPLETED,
+    };
+}
+
+export const clearCompleted = () => {
+    return {
+        type: CLEARCOMPLETED,
+    };
+}
